7bbed1c85e8bbf28c84250d8c1a99888d7af7ecc
alquist4121 <45760211+sekizawaryo@users.noreply.github.com>
2022-11-03 23:20:20 +0900

    initial commit

---
+++ Kogiri/Kogiri/KogiriApp.swift
@@ -0,0 +1,17 @@
+//
+//  KogiriApp.swift
+//  Kogiri
+//
+//  Created by alquist4121 on 2022/11/03.
+//
+
+import SwiftUI
+
+@main
+struct KogiriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
